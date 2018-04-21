4408f5a8c9da40177ce29d22afa2150ba17dceb3
César Ramos <cesar3r2@gmail.com>
2018-04-21 06:33:10 -0400

    Cambio en los planes

--- inicio.php
+++ inicio.php
@@ -3,7 +3,7 @@
             <div class="row">
                 <div class="col-md-3">
                     <h3>Nuestros planes</h3>
-                    <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+                    <p>Queremos colocar este texto para que se entienda y no como dice la plantilla.as.</p>
                     <div class="btn-group">
                         <a class="btn btn-danger" href="#scroller" data-slide="prev"><i class="icon-angle-left"></i></a>
                         <a class="btn btn-danger" href="#scroller" data-slide="next"><i class="icon-angle-right"></i></a>
